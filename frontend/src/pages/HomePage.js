7ebb5673f13778cc8a0e2680de5df466a8b6a67d
vinit mittal <vinitmittal295@gmail.com>
2024-10-26 21:36:58 +0530

    added

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,12 +1,31 @@
 import React from "react"
-import{container} from "@chakra-ui/react"
+import{Container,Box,Text} from "@chakra-ui/react"
 const HomePage=()=>{
 
 
     return (
-    <container>
-        
-   </container>
+    <Container maxW="xl" centerContent>
+        <Box 
+        d="flex"
+        // justifyContent="center"
+        justifyContent="center" 
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+        >
+            <Text fontSize='4xl' fontFamily="Work sans" color="black">
+                Talk-A-tive
+            </Text>
+        </Box>
+
+        <Box
+        bg="white" w="100%" p={4} borderRadius="lg" color="black" borderWidth="1px">
+            
+        </Box>
+   </Container>
    )
     
 }
