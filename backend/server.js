7b681f64a9117f2d3639b9ea8e77b065eba85b72
vinit mittal <vinitmittal295@gmail.com>
2024-11-08 17:12:35 +0530

    added

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
 const express=require("express")
 const dotenv=require("dotenv")
 const {chats} =require("./data/data.js")
-const app=express()
+const connectDB = require("./config/db.js")
+const colors=require("colors")
 dotenv.config()
-app.get("/",(req,res)=>{
-    res.send("api is running")
-})
 
-app.get("/api/chat",(req,res)=>{
-    res.send(chats)
-})
+const app=express()
+app.use(express.json())
+
+
+connectDB()
+
+
+const userRoutes=require("./Routes/userRoutes.js")
+app.use("/api/user",userRoutes)
 
 const PORT=process.env.PORT || 5000
-app.listen(PORT,console.log(`server is running on port ${PORT}`))
+app.listen(PORT,console.log(`server is running on port ${PORT}`.yellow.bold))
